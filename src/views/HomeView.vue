e5bc7ad2635b00431f904262a2a7e1be0cfdbd47
davidannebicque <david.annebicque@gmail.com>
2025-03-23 13:49:31 +0100

    typo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,7 @@
         <ul>
           <li> En étudiant la documentation de l'API, affichez les personnages et les liens suivants/précédents pour naviguer entre les pages. Les boutons seront masqués si la page suivante/précédente n'existe pas.</li>
           <li> Chaque personnage doit être affiché dans un composant.</li>
-          <li> Ajouter un bouton pour afficher les détails d'un personnage, qui chargera une partie de la page avec les éléments via une route imbriquée.</li>
+          <li> Ajoutez un bouton pour afficher les détails d'un personnage, qui chargera une partie de la page avec les éléments via une route imbriquée.</li>
           <li> Chaque personnage aura une case à cocher pour le marquer comme "selectionné". Les pesonnages selectionnés seront stockés dans une variable du parent et affichés sous forme d'une liste.</li>
           <li> Completez la page AboutView avec votre nom et Groupe.</li>
           <li> Complétez le nombre d'items dans l'API pour afficher le nombre total de personnages.
